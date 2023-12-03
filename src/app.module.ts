d02d90b608835862b7412b7c2d7db1ee13b016eb
Omar Fahmy <ofahmy1234@gmail.com>
2023-12-03 08:26:32 +0200

    feat(prisma): set up module Globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
