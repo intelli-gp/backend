7eb772ab13c91a77827c5fef353f7bd5d776669a
Omar Fahmy <ofahmy1234@gmail.com>
2023-12-03 08:51:49 +0200

    feat(config): setup module to be used globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
