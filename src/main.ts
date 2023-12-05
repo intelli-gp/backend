e8b31b2e684ce8cdf4596e45bfe5510aee8bb4fb
Omar Fahmy <ofahmy1234@gmail.com>
2023-12-05 12:37:53 +0200

    feat(cors): set up and use cors middleware

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.use(cors());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
