6830244714d87fd43e29f3063fac772bcd6820df
Omar Fahmy <ofahmy1234@gmail.com>
2023-12-05 16:22:00 +0200

    feat(auth): enable at guard globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AtGuard } from './modules/auth/guards/access.jwt.guard';
 
 @Module({
   imports: [
@@ -12,6 +14,11 @@ import { AuthModule } from './modules/auth/auth.module';
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+  ],
 })
 export class AppModule {}
