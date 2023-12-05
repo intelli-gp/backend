a05a54e07be8cfe4143cee1aed74a07f44245216
Omar Fahmy <ofahmy1234@gmail.com>
2023-12-05 16:26:01 +0200

    fix(core): enable cors correctly and cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cors from 'cors';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  app.use(cors());
+  await app.listen(3333);
+  app.enableCors();
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
