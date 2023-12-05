dd2bdffa805af73a8f6f504bd9692b660a2c9196
Omar Fahmy <ofahmy1234@gmail.com>
2023-12-05 16:22:36 +0200

    feat(auth): provide the passport strategy as module providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { GoogleStrategy } from './strategy/google.strategy';
+import { AccessJwtStrategy } from './strategy/access.jwt.strategy';
+import { RefreshJwtStrategy } from './strategy/refresh.jwt.strategy';
 
 @Module({
   imports: [JwtModule.register({})],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    GoogleStrategy,
+    AccessJwtStrategy,
+    RefreshJwtStrategy,
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
