17e5968f67758481cd3a9aad30a0f2dcaab6deb6
Omar Fahmy <ofahmy1234@gmail.com>
2023-12-06 08:53:05 +0200

    feat: (swagger) : set up swagger examples for signup dto

--- src/modules/auth/dto/signup.dto.ts
+++ src/modules/auth/dto/signup.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsDate,
@@ -11,37 +12,46 @@ import {
 } from 'class-validator';
 
 export class SignUpDto {
+  @ApiProperty({ required: true, example: 'John32' })
   @IsNotEmpty()
   @MinLength(4)
   username: string;
 
+  @ApiProperty({ required: true, example: 'John' })
   @IsNotEmpty()
   @MinLength(3)
   fname: string;
 
+  @ApiProperty({ required: true, example: 'Doe' })
   @IsNotEmpty()
   @MinLength(3)
   lname: string;
 
+  @ApiProperty({ required: true, example: 'johndoe@gmail.com' })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ required: true, example: 'Testpassword@1234' })
   @IsNotEmpty()
   @MinLength(8)
   password: string;
 
+  @ApiProperty({ required: true, example: '+xx1050790880' })
   @IsNotEmpty()
   @IsPhoneNumber()
   phoneNumber: string;
 
+  @ApiProperty({ required: true, example: '2020-01-01' })
   @IsNotEmpty()
   @IsDate()
   dob: Date;
 
+  @ApiProperty({ required: true, example: 'https://image.com' })
   @IsUrl()
   image: string;
 
+  @ApiProperty({ required: true, example: ['interest1', 'interest2'] })
   @IsNotEmpty()
   @IsArray()
   @ValidateNested()
