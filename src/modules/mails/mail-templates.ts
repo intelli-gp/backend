333163b9da898194f5478da6007b28f2d11a65de
Abd-El-Rahman Nasser <abdonasser865@gmail.com>
2023-12-10 18:00:52 +0200

    fix(mail): base_url -> server_url

--- src/modules/mails/mail-templates.ts
+++ src/modules/mails/mail-templates.ts
@@ -156,7 +156,7 @@ export default class templates {
             <!-- start copy -->
             <tr>
               <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                <p style="margin: 0;">Tap the button below to confirm your email address. If you didn't create an account with <a href="${process.env.BASE_URL}/api/auth/reset/${username}/${token}">Paste</a>, you can safely delete this email.</p>
+                <p style="margin: 0;">Tap the button below to confirm your email address. If you didn't create an account with <a href="${process.env.SERVER_URL}/api/auth/reset/${username}/${token}">Paste</a>, you can safely delete this email.</p>
               </td>
             </tr>
             <!-- end copy -->
@@ -170,7 +170,7 @@ export default class templates {
                       <table border="0" cellpadding="0" cellspacing="0">
                         <tr>
                           <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                            <a href="${process.env.BASE_URL}/api/auth/verify/${username}/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify your mail</a>
+                            <a href="${process.env.SERVER_URL}/api/auth/verify/${username}/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify your mail</a>
                           </td>
                         </tr>
                       </table>
@@ -185,7 +185,7 @@ export default class templates {
             <tr>
               <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
                 <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
-                <p style="margin: 0;"><a href="${process.env.BASE_URL}/api/auth/verify/${username}/${token}" target="_blank">${process.env.BASE_URL}/api/auth/verify/${username}/${token}</a></p>
+                <p style="margin: 0;"><a href="${process.env.SERVER_URL}/api/auth/verify/${username}/${token}" target="_blank">${process.env.SERVER_URL}/api/auth/verify/${username}/${token}</a></p>
               </td>
             </tr>
             <!-- end copy -->
@@ -390,7 +390,7 @@ export default class templates {
             <!-- start copy -->
             <tr>
               <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                <p style="margin: 0;">Tap the button below to reset your password. If you didn't reset password with <a href="${process.env.BASE_URL}/api/auth/reset/${email}/${token}">Paste</a>, you can safely delete this email.</p>
+                <p style="margin: 0;">Tap the button below to reset your password. If you didn't reset password with <a href="${process.env.SERVER_URL}/api/auth/reset/${email}/${token}">Paste</a>, you can safely delete this email.</p>
               </td>
             </tr>
             <!-- end copy -->
@@ -404,7 +404,7 @@ export default class templates {
                       <table border="0" cellpadding="0" cellspacing="0">
                         <tr>
                           <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                            <a href="${process.env.BASE_URL}/api/auth/reset-password/${email}/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Reset your password</a>
+                            <a href="${process.env.SERVER_URL}/api/auth/reset-password/${email}/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Reset your password</a>
                           </td>
                         </tr>
                       </table>
@@ -419,7 +419,7 @@ export default class templates {
             <tr>
               <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
                 <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
-                <p style="margin: 0;"><a href="${process.env.BASE_URL}/api/auth/reset-password/${email}/${token}" target="_blank">${process.env.BASE_URL}/api/auth/reset-password/${email}/${token}</a></p>
+                <p style="margin: 0;"><a href="${process.env.SERVER_URL}/api/auth/reset-password/${email}/${token}" target="_blank">${process.env.SERVER_URL}/api/auth/reset-password/${email}/${token}</a></p>
               </td>
             </tr>
             <!-- end copy -->
