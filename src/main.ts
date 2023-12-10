8f8260e6a6703068af5eb40f2a5549fc23d2d12c
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-12-10 18:26:05 +0200

    feat: enable CORS for localhost:5173

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const config = new ConfigService();
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
