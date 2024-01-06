7036719ee5ee273a7f7de478fb08324a03630c8c
Omar Fahmy <ofahmy1234@gmail.com>
2024-01-06 21:47:01 +0200

    feat(pipes):
    - add a global pipe to trim strings of all incoming objects

---
+++ src/utils/pipes/trim-string.pipe.ts
@@ -0,0 +1,17 @@
+import { Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class TrimStringsPipe implements PipeTransform {
+  transform(value: any) {
+    if (typeof value === 'object') {
+      for (const key in value) {
+        if (typeof value[key] === 'string') {
+          value[key] = value[key].trim();
+        } else if (typeof value[key] === 'object') {
+          this.transform(value[key]);
+        }
+      }
+    }
+    return value;
+  }
+}
