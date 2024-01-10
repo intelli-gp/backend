02384fbdf10e677323aaa36641e4595751866707
Omar Fahmy <ofahmy1234@gmail.com>
2024-01-10 12:48:05 +0200

    fix(prisma-exception):
    - add p2000 for exceeding size error handling

--- src/exception-filters/utils/prisma-errors.ts
+++ src/exception-filters/utils/prisma-errors.ts
@@ -6,9 +6,11 @@ export const PrismaErrorMessage = {
   P2005: 'invalid field value',
   P2025:
     'An operation failed because it depends on one or more records that were required but not found.',
+  p2000: 'input exceeded field size',
 };
 
 export const PrismaErrorStatusCode = {
+  p2000: HttpStatus.BAD_REQUEST,
   P2002: HttpStatus.BAD_REQUEST,
   P2003: HttpStatus.BAD_REQUEST,
   P2005: HttpStatus.BAD_REQUEST,
