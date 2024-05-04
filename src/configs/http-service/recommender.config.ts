ec2958c4cc412024bd02f155c7bc51a59517e0af
Abdo Nasser <abdonasser865@gmail.com>
2024-05-04 20:17:49 +0300

    feat(recommendations): auth recommender with API Key

---
+++ src/configs/http-service/recommender.config.ts
@@ -0,0 +1,18 @@
+import { HttpModuleOptions, HttpModuleOptionsFactory } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class HttpRecommenderConfigService implements HttpModuleOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+  createHttpOptions(): HttpModuleOptions {
+    return {
+      baseURL: this.configService.get('RECOMMENDER_BASE_URL'),
+      timeout: 20000,
+      maxRedirects: 5,
+      headers: {
+        api_key: this.configService.get('RECOMMENDER_API_KEY'),
+      },
+    };
+  }
+}
