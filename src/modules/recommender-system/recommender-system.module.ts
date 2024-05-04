82ea4c7e62e47e2ef9d7c894a4d7e25422d5ccf8
Abdo Nasser <abdonasser865@gmail.com>
2024-05-04 20:18:34 +0300

    missed from latest commit

--- src/modules/recommender-system/recommender-system.module.ts
+++ src/modules/recommender-system/recommender-system.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { RecommenderSystemController } from './recommender-system.controller';
 import { RecommenderSystemService } from './recommender-system.service';
 import { HttpModule } from '@nestjs/axios';
+import { HttpRecommenderConfigService } from '../../configs/http-service/recommender.config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule.registerAsync({
+      useClass: HttpRecommenderConfigService,
+    }),
+  ],
   controllers: [RecommenderSystemController],
   providers: [RecommenderSystemService],
 })
