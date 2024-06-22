9ede167ce0dbf7584fd61f9b47bd7fa4751eed07
Omar <ofahmy1234@gmail.com>
2024-06-23 00:34:29 +0300

    feat(subscription): check in guard with stripe service itself instead of plan id

--- src/common/guards/pro-subscription.guard.ts
+++ src/common/guards/pro-subscription.guard.ts
@@ -1,18 +1,29 @@
-import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import {
+    Injectable,
+    CanActivate,
+    ExecutionContext,
+    ForbiddenException,
+} from '@nestjs/common';
+import { user } from '@prisma/client';
 import { Observable } from 'rxjs';
+import { StripeService } from 'src/modules/payment/stripe.service';
 
 @Injectable()
 export class ProSubscriptionGuard implements CanActivate {
-    canActivate(
-        context: ExecutionContext,
-    ): boolean | Promise<boolean> | Observable<boolean> {
+    constructor(private readonly stripeService: StripeService) {}
+    async canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest();
-        const user = request.user;
+        const stripeCustomerId = (request?.user as user)?.stripe_customer_id;
 
-        if (user && user.plan_id) {
-            return user.plan_id == 2;
+        const subscription =
+            await this.stripeService.getCustomerSubscriptions(stripeCustomerId);
+
+        if (subscription && subscription.data.length > 0) {
+            return subscription.data[0].status === 'active';
         }
 
-        throw new ForbiddenException('You need to have a pro subscription to access this feature');
+        throw new ForbiddenException(
+            'You need to have a pro subscription to access this feature',
+        );
     }
 }
--- src/modules/ai-service/ai-service.module.ts
+++ src/modules/ai-service/ai-service.module.ts
@@ -3,6 +3,12 @@ import { AiServiceController } from './ai-service.controller';
 import { AiServiceService } from './ai-service.service';
 import { HttpModule } from '@nestjs/axios';
 import { AiServiceHttpConfig } from 'src/configs/http-service/ai-service.config';
+import { ProSubscriptionGuard } from 'src/common/guards/pro-subscription.guard';
+import { StripeService } from '../payment/stripe.service';
+import { UsersService } from '../users/users.service';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../prisma/prisma.service';
+import { TagsService } from '../tags/tags.service';
 
 @Module({
     imports: [
@@ -11,6 +17,13 @@ import { AiServiceHttpConfig } from 'src/configs/http-service/ai-service.config'
         }),
     ],
     controllers: [AiServiceController],
-    providers: [AiServiceService],
+    providers: [
+        AiServiceService,
+        StripeService,
+        UsersService,
+        ConfigService,
+        PrismaService,
+        TagsService,
+    ],
 })
 export class AiServiceModule {}
